7b45de0d37f4f7e976bcc17d477710f02501458c
Bob at HP <andy.porter@landregistry.gsi.gov.uk>
2013-04-25 11:06:40 -0700

    First empty app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dtc::Application.config.secret_token = 'bf966771cea22217383049c178100181f0fc5b358ae5043c204f82322c73e25879529c0305f48656bfa7d35295f51ae29875d4e972da9f203c8e030aae2a0b15'
